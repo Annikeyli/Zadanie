3beba9b2f72cfd8c15c9880e0930274432cc5d03
Annikeyli <anna20061010@yandex.ru>
2025-04-09 14:46:33 +0300

    zadanie1.cpp

--- zadanie1.cpp
+++ zadanie1.cpp
@@ -30,36 +30,19 @@ int main()
     ofstream outbin("a.bin", ios_base::binary);
     arr.writeBin(outbin);
     ifstream inbin("a.bin", ios_base::binary);
- //   arr.sort(compInt);
-   /* cout << "\after sort\n" << arr;
-    ofstream out("int.bin", ios::binary);
-    arr.writeBin(out);
-    out.close();
-    ifstream out("int.bin", ios::binary);
-    My_Array<int>arr1;
-    arr1.readBin(in);
-    cout << "\n from bin file \n" << arr1;    */
+    int month;
+    cout << "\nEnter month: ";
+    
+    cin >> month;
+    bool found = false;
+    for (int i = 0; i < 8; i++) {
+        if (arr[i].data[1] == month) {
+            std::cout << arr[i] << "\n";
+            found = true;
+        }
+    }
+    if (!found) {
+        std::cout << "No people born in this month.\n";
+    }
+    return 0;
 }
-
-
-
-/*ifstream in("dan.txt");
-    if (!in)cout << "\nfile not open\n";
-    Znak gr[8];
-    ifstream in1("C:\\Users\\aakrylova3\\source\\repos\\zadanie1\\dan.txt");
-    if (!in)cout << "\nfile not open\n";
-    for (int i = 0; i < 8; i++)
-        in1 >> gr[i];
-    for (int i = 0; i < 8; i++)
-        cout << gr[i] << "\n";
-
-    My_Array<int> arr;
-    cin >> arr;
-    cout << arr;
-    My_Array<int> help;
-    help = arr;
-    cout << "\n help\n" << help;*/
-
-    //Znak  z("Ivanov", "Ivan", "deva", 2, 3, 1992);
-        //Znak  z1("Ivanov", "Ivan", "deva", new int[3] {2, 3, 1992}); // same  с массивом
-        //cout << z << "\n";     // с массивом + << z1 << "\n";
